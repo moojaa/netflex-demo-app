bae6f481edb49559f813296fd8c9f056c5b6e9dd
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-06 22:32:25 +0900

    배너추가

--- src/pages/homepage/components/banner/Banner.jsx
+++ src/pages/homepage/components/banner/Banner.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies'
+import Alert from 'react-bootstrap/Alert';
+import "./Banner.style.css"
 
 const Banner = () => {
-    const {data,isLoading,error} = usePopularMoviesQuery()
+    const {data,isLoading,isError,error} = usePopularMoviesQuery()
     console.log('ddd',data)
     if(isLoading){
         <h1>loading</h1>
     }
-    if(error){
-        
+    if(isError){
+        <Alert variant='danger'>{error.message}</Alert>
     }
   return (
-    <div>Banner</div>
+    <div style={{
+      backgroundImage:"url("+`https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data?.results[0].backdrop_path}`+")"
+    }}
+    className='banner'
+    >
+      <div className='text-white banner-text-area'>
+        <h1>{data?.results[0].title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
   )
 }
 

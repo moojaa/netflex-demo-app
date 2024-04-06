601d6153bd7933abc0ad9d3861eafdb1ba24c3c0
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-06 23:39:11 +0900

    무비카드 업데이트

--- src/pages/homepage/components/movieCard/MovieCard.jsx
+++ src/pages/homepage/components/movieCard/MovieCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Badge from 'react-bootstrap/Badge';
 import './MovieCard.style.css'
+import { faImdb } from '@fortawesome/free-brands-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MovieCard = ({ movie }) => {
     return (
@@ -9,14 +12,16 @@ const MovieCard = ({ movie }) => {
             className='movie-card'
         >
             <div className='overlay'>
-                <h1>{movie.title}</h1>
-                {movie.genre_ids.map((id) => (
-                    <Badge bg="danger">{id}</Badge>
-                ))}
                 <div>
-                    <div>{movie.vote_average}</div>
-                    <div>{movie.popularity}</div>
-                    <div>{movie.adult ? 'over18' : 'under18'}</div>
+                <h3>{movie.title}</h3>
+                {movie.genre_ids.map((id) => (
+                    <Badge bg="danger me-1">{id}</Badge>
+                ))}
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <div className='movie-card-font'><FontAwesomeIcon icon={faImdb} className='me-1' />{movie.vote_average}</div>
+                    <div className='movie-card-font'><FontAwesomeIcon icon={faUsers} className='me-1' />{Math.floor(movie.popularity)}</div>
+                    <div className='movie-card-font'>{movie.adult ? <div className='bg-danger rounded-circle adult-style'>18</div> : <div className='bg-warning rounded-circle text-black adult-style'>all</div>}</div>
                 </div>
             </div>
         </div>
